3b8ddc938e02b9e25551a65d88aa104b1519f77a
rubayatrifat <rubayatislam17@gmail.com>
2026-07-05 10:50:17 +0600

    To-Do list with Local Storage

--- 06. To-Do List JS (Local Storage)/main.js
+++ 06. To-Do List JS (Local Storage)/main.js
@@ -4,6 +4,40 @@ const errMsg = document.querySelector("#error-msg")
 const errClose = document.querySelector(".error-close-btn")
 const toDoList = document.querySelector("#todo-list")
 const filterBtn = document.querySelectorAll(".filter-btn")
+if (localStorage.getItem("toDoInfo") === null) {
+    localStorage.setItem("toDoInfo", JSON.stringify([]));
+}
+function loadData() {
+    let itemCount = JSON.parse(localStorage.getItem("toDoInfo")).length
+    let noItemMsg = document.createElement("span")
+    if (itemCount === 0) {
+        noItemMsg.style.color = "rgb(140 159 185)"
+        noItemMsg.textContent = "No To-Do yet!!";
+
+        toDoList.appendChild(noItemMsg);
+    } else {
+        toDoList.innerHTML = ""
+        let toDoTxt = JSON.parse(localStorage.getItem("toDoInfo"))
+        for (let i = 0; i < itemCount; i++) {
+            let theItem = `
+            <li class="todo-item" data-id="${toDoTxt[i].id}">
+                <span class="task-text">${toDoTxt[i].text}</span>
+                <button class="delete-btn">&times;</button>
+            </li>`
+            if (toDoTxt[i].isPending === false) {
+                theItem = `
+                <li class="todo-item completed" data-id="${toDoTxt[i].id}">
+                    <span class="task-text">${toDoTxt[i].text}</span>
+                    <button class="delete-btn">&times;</button>
+                </li>`
+            }
+            toDoList.insertAdjacentHTML("afterbegin", theItem);
+        }
+    }
+}
+
+loadData()
+
 
 // =========== Adding a todo ============
 
@@ -29,16 +63,16 @@ function checkingInput() {
         return toDoInput.value
     }
 }
-
-// Adding a todo to the list
-function addToDo(toDoTxt) {
-    let theItem = `           
-    <li class="todo-item">
-        <span class="task-text">${toDoTxt}</span>
-        <button class="delete-btn">&times;</button>
-    </li>`
-
-    toDoList.insertAdjacentHTML("afterbegin", theItem)
+// Adding Item to the local storage
+function addItemLocalSto(title) {
+    let theItem = {
+        id: Date.now(),
+        text: title,
+        isPending: true
+    }
+    let currentList = JSON.parse(localStorage.getItem("toDoInfo"))
+    currentList.push(theItem)
+    localStorage.setItem("toDoInfo", JSON.stringify(currentList))
 }
 
 // Submiting todo
@@ -46,7 +80,8 @@ toDoAddBtn.addEventListener("click", (evt) => {
     evt.preventDefault()
     const inputValue = checkingInput()
     if (inputValue) {
-        addToDo(inputValue)
+        addItemLocalSto(inputValue)
+        loadData()
         toDoInput.value = ""
     }
 })
@@ -54,13 +89,34 @@ toDoAddBtn.addEventListener("click", (evt) => {
 
 
 // ================ To-Do Items Functionalities ===============
+
+function removeItem(id) {
+    let localStoArr = JSON.parse(localStorage.getItem("toDoInfo"))
+    let newData = localStoArr.filter(item => item.id !== Number(id))
+    localStorage.setItem("toDoInfo", JSON.stringify(newData))
+}
+
+function completedItem(id) {
+    let localStoArr = JSON.parse(localStorage.getItem("toDoInfo"))
+    localStoArr.forEach(item => {
+        if (item.id === Number(id)) {
+            item.isPending = !item.isPending
+        }
+    })
+    localStorage.setItem("toDoInfo", JSON.stringify(localStoArr))
+}
+
 toDoList.addEventListener("click", (evt) => {
     if (evt.target.classList.contains("delete-btn")) {
+        removeItem(evt.target.parentElement.dataset.id)
         evt.target.parentElement.remove()
+        loadData()
     }
 
     if (evt.target.classList.contains("task-text")) {
+        completedItem(evt.target.parentElement.dataset.id)
         evt.target.parentElement.classList.toggle("completed")
+        loadData()
     }
 })
 
@@ -79,10 +135,10 @@ filterBtn.forEach(item => {
 
         const toDoItems = document.querySelectorAll(".todo-item")
         toDoItems.forEach(singleItem => {
-            if(currentFilter === "all") {
+            if (currentFilter === "all") {
                 singleItem.classList.remove("hide")
             } else if (currentFilter === "pending") {
-                if(singleItem.classList.contains("completed")) {
+                if (singleItem.classList.contains("completed")) {
                     singleItem.classList.add("hide")
                 } else {
                     singleItem.classList.remove("hide")
