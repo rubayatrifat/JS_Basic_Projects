237c0a08444763f52f00261d282c1140fbd9cf59
rubayatrifat <rubayatislam17@gmail.com>
2026-07-06 22:46:03 +0600

    Js Search Feature

--- 07. JS Search Feature/main.js
+++ 07. JS Search Feature/main.js
@@ -1,16 +1,41 @@
-// Accesing Form HTML 
-const imgInput = document.querySelector(".image-url")
-const userName = document.querySelector(".user-name")
-const userAddress = document.querySelector(".user-address")
-const category = document.querySelectorAll(".category-rad")
+const userData = {
+    department: "Tech Team",
+    users: [
+        { id: 1, name: "Anik Rahman", role: "Frontend Developer" },
+        { id: 2, name: "Sajid Hasan", role: "Backend Developer" },
+        { id: 3, name: "Tamanna Islam", role: "UI/UX Designer" },
+        { id: 4, name: "Naimur Rahman", role: "App Developer" },
+        { id: 5, name: "Rokeya Sultana", role: "Data Scientist" }
+    ]
+}
+const userList = document.querySelector("#userList")
+const searchInput = document.querySelector("#searchInput")
+let users = userData.users
 
-// Accessing Card HTML
+function loadData(dataArr) {
+    userList.innerHTML = ""
+    dataArr.forEach(item => {
+        let cardUi = `<li class="user-item">
+                        <span class="user-name">${item.name}</span>
+                        <span class="user-role">${item.role}</span>
+                     </li>`
 
+        userList.insertAdjacentHTML("beforeend", cardUi)
+    }) 
+}
 
+loadData(users)
 
+function filterData(inputedValue) {
+    let searchedItms = users.filter(item => {
+        return item.name.toLowerCase().includes(inputedValue.toLowerCase()) || item.role.toLowerCase().includes(inputedValue.toLowerCase())
+    })
 
-category.forEach(item => {
-    if(item.checked === true) {
-        console.log(item.dataset.category)
-    }
+    loadData(searchedItms)
+}
+
+searchInput.addEventListener("input", evt => {
+    filterData(evt.target.value)
+
 })
+
