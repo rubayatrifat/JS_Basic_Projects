5dba5dd75b6e2079953cddb8f365a66f3338ad55
rubayatrifat <rubayatislam17@gmail.com>
2026-07-07 15:44:32 +0600

    Contact List Form validation done

--- 08. Contact List Manager/main.js
+++ 08. Contact List Manager/main.js
@@ -8,7 +8,7 @@ const imgInput = document.querySelector(".image-url")
 const userNameInput = document.querySelector(".user-name")
 const userAddressInput = document.querySelector(".user-address")
 const noteInput = document.querySelector("#noteInput")
-const categoryInput = document.querySelectorAll(".category-rad")
+const categoryInputs = document.querySelectorAll(".category-rad")
 const createNoteBtn = document.querySelector(".create-note")
 const errMsg = document.querySelector("#errorMsg")
 const imgUrlRegex = /\.(jpeg|jpg|gif|png|webp)$/i
@@ -37,24 +37,78 @@ window.addEventListener('click', (e) => {
 function showErr(messege) {
     errMsg.classList.remove("hidden")
     errMsg.textContent = messege
+    setTimeout(() => {
+        errMsg.classList.add("hidden")
+    }, 3000)
 }
+
+// ========== Checking inputed charecter number
+
+// Live changing charecter count
+formModal.addEventListener("input", evt => {
+    let inputedCharLen = evt.target.value.length
+    let charCountElm = document.getElementById(evt.target.id.replace("Input", "Counter"))
+    if (!charCountElm) return
+    charCountElm.textContent = inputedCharLen
+    if(inputedCharLen > evt.target.dataset.maxchar) {
+        charCountElm.style.color = "rgb(239 68 68)"
+    } else {
+        charCountElm.style.color = "rgb(107 114 128)"
+    }
+})
+
+
+function checkCharNum(targetEle, maxChar, name) {
+    if (targetEle.value.length > maxChar) {
+        showErr(`You can only input ${maxChar} charecter in ${name}!!`)
+        return false
+    }
+}
+
 
 // Checking valid input (function)
 function checkInputs() {
     if (imgInput.value.trim() === "" || !imgUrlRegex.test(imgInput.value)) {
         showErr("Please fill out the url field properly!!")
         return false
-    } else if (userNameInput.value.trim() === "" || userAddressInput.value.trim() === "" || userNameInput.value.trim() === "" || noteInput.value.trim() === "") {
+    } else if (userNameInput.value.trim() === "" || userAddressInput.value.trim() === ""|| noteInput.value.trim() === "") {
         showErr("Please fill out the input field!!")
+        return false
     }
+    let categoryChecked = Array.from(categoryInputs).some(item => item.checked)
+    if(!categoryChecked) {
+        showErr("Please select a category!!")
+        return false
+    }
+    checkCharNum(userNameInput, userNameInput.dataset.maxchar, "Name Input Field")
+    checkCharNum(userAddressInput, userAddressInput.dataset.maxchar, "Address Input Field")
+    checkCharNum(noteInput, noteInput.dataset.maxchar, "Note Input Field")
 }
+
+// Reseting the form
+function resetForm() {
+    // Reseting input
+    imgInput.value = ""
+    userNameInput.value = ""
+    userAddressInput.value = ""
+    noteInput.value = ""
+    // Reseting input charecter counter
+    nameCounter.textContent = "0"
+    homeCounter.textContent = "0"
+    noteCounter.textContent = "0"
+    // Reseting input counter color
+    nameCounter.style.color = "rgb(107 114 128)"
+    homeCounter.style.color = "rgb(107 114 128)"
+    noteCounter.style.color = "rgb(107 114 128)"
+    // Closing the modal
+    formModal.classList.add('hidden');
+    formModal.classList.remove('flex');
+}
+
 // Adding a new note card
 createNoteBtn.addEventListener("click", evt => {
     checkInputs()
+    if (checkInputs() === false) return;
+    resetForm()
 })
 
-// category.forEach(item => {
-//     if(item.checked === true) {
-//         console.log(item.dataset.category)
-//     }
-// })
